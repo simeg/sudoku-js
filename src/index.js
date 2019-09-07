7579809b5454d4e1625bdf56294a3b74c91973a9
Simon Egersand <s.egersand@gmail.com>
2019-09-07 12:26:00 +0200

    Add erase button

--- src/index.js
+++ src/index.js
@@ -13,15 +13,7 @@ app.set("views", __dirname + "/views");
 
 app.get("/", async (req, res) => {
   const board = adjustDifficulty(generateSudokuBoard(), DIFFICULTIES.easy);
-  const row1 = board[0];
-  const row2 = board[1];
-  const row3 = board[2];
-  const row4 = board[3];
-  const row5 = board[4];
-  const row6 = board[5];
-  const row7 = board[6];
-  const row8 = board[7];
-  const row9 = board[8];
+  const [row1, row2, row3, row4, row5, row6, row7, row8, row9] = board;
   res.render("index", {
     row1,
     row2,
